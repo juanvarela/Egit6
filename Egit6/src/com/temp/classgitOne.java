6bd8525da55141870d17123df1f647b6ced56069
Juan Varela <youremailaddress>
2015-04-15 11:29:54 -0400

    Class one and two print statements

---
+++ Egit6/src/com/temp/classgitOne.java
@@ -0,0 +1,10 @@
+package com.temp;
+
+public class classgitOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is the first class");
+	}
+
+}
---
+++ Egit6/src/com/temp/classgitTwo.java
@@ -0,0 +1,10 @@
+package com.temp;
+
+public class classgitTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the class two");
+	}
+
+}
